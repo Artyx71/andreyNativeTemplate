6f3d46a981af18d409d26ba436710e5d434d565c
Andrew Gabaraev <androgabar@gmail.com>
2025-02-26 11:29:25 +0300

    slider

---
+++ script.js
@@ -0,0 +1,27 @@
+const slides = document.querySelector('.slides')
+const slide = document.querySelectorAll('.slide')
+const totalLength = slide.length
+let index = 0
+const prev = document.querySelector('.prev')
+const next = document.querySelector('.next')
+const slideWidth = slide[0].clientWidth
+
+next.addEventListener('click', () => {
+	index++
+	if (index >= totalLength) {
+		index = 0
+	}
+	updateSlide()
+})
+
+prev.addEventListener('click', () => {
+	index--
+	if (index < 0) {
+		index = totalLength - 1
+	}
+	updateSlide()
+})
+
+function updateSlide() {
+	slides.style.transform = `translateX(-${index * slideWidth}px)`
+}
